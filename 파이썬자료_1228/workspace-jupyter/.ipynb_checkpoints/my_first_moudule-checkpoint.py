d37559b020b272eef512fe81e3978eda11723db2
AntoinoA12 <gyqlsdlrj@gmail.com>
2023-02-09 18:16:14 +0900

    delete

--- 파이썬자료_1228/workspace-jupyter/.ipynb_checkpoints/my_first_moudule-checkpoint.py
+++
@@ -1,4 +0,0 @@
-#파일명 = my_first_module
-
-def my_function():
-    print('This is my first module')
--- 파이썬자료_1228/workspace-jupyter/my_area.py
+++
@@ -1,9 +0,0 @@
-
-PI=3.14
-
-def squar_area(a):
-    return a**2
-
-def circle_area(r):
-    return PI*r**2
-
