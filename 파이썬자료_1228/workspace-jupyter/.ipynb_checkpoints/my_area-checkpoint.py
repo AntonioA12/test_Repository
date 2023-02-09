10e09af794b57f00e3c8552a4eac284dac735f9c
AntoinoA12 <gyqlsdlrj@gmail.com>
2023-02-09 17:32:23 +0900

    ToMain

---
+++ 파이썬자료_1228/workspace-jupyter/.ipynb_checkpoints/my_area-checkpoint.py
@@ -0,0 +1,9 @@
+
+PI=3.14
+
+def squar_area(a):
+    return a**2
+
+def circle_area(r):
+    return PI*r**2
+
---
+++ 파이썬자료_1228/workspace-jupyter/my_first_moudule.ipynb
@@ -0,0 +1,4 @@
+#파일명 = my_first_module
+
+def my_function():
+    print('This is my first module')
